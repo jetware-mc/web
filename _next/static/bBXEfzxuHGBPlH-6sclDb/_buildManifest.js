1fd68c8fa473cb3def92b842405f6ebff19b1cea
theDevJade <jadeisstolen@gmail.com>
2023-11-27 20:55:52 +0000

    Deploying to gh-pages from @ jetware-mc/web@1c89b300493fbd3517041745643ac9783a8463ef 🚀

---
+++ _next/static/bBXEfzxuHGBPlH-6sclDb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/703-ca214ca1ce3cec7d.js","static/css/a11da8c9bd253f0e.css","static/chunks/pages/index-141fa7f29e9e51b8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
